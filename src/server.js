b56e32bb933d118f96ea5f3d8f9d922cf13057d6
icatherena <beresovskyc@gmail.com>
2023-11-17 17:40:36 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -18,6 +18,13 @@ const options = {
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/api', router);
 
 const startServer = async () => {
