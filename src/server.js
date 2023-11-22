96f38cec4159b0592b25b56a5b4f50cca568e34f
icatherena <beresovskyc@gmail.com>
2023-11-22 12:51:44 -0300

    working on auth client side

--- src/server.js
+++ src/server.js
@@ -20,10 +20,13 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true'); // If using credentials
     next();
 });
+
+app.options('*', cors());
 
 app.use('/api', router);
 
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,7 +6,7 @@ const secretKey = process.env.TOKEN_SECRET;
 function authenticated(req, res, next) {
     // Obtiene el token desde el header
     const [bearer, token] = (req.headers.authorization)?.split(' ') ?? [];
-
+    
     // Formato del header
     if (!bearer || !token || bearer !== 'Bearer') {
         return res.status(401).json({ message: 'MISSING TOKEN' });
