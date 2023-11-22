7eb488d13672b647d07fc050c9c9a045b30f15f5
icatherena <beresovskyc@gmail.com>
2023-11-22 13:02:47 -0300

    working on auth client side

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const connectDB = require('../db/database');
+const cors = require('cors'); 
 const mongoose = require('mongoose');
 
 const router = require('./router/index');
