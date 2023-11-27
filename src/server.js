21054cbc66f0fc624027bdb4c4d15afd9fb66b74
icatherena <beresovskyc@gmail.com>
2023-11-27 09:12:14 -0300

    enabling server to request from deployed client origin

--- src/server.js
+++ src/server.js
@@ -18,13 +18,20 @@ const options = {
 
 app.use(express.json());
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:5173, https://invert-box-ecommerce.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true'); // If using credentials
     next();
-});
+}); */
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://invert-box-ecommerce.netlify.app'],
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+}));
 
 app.options('*', cors());
 
